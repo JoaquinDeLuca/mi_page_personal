9bad60eaed1592563d0bdf6a91619b0d4b9d341b
JoaquinDeLuca <joaqui222@gmail.com>
2022-11-03 20:07:03 -0300

    se agrega info

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import './Footer.css'
 
 export default function Footer() {
+
+  const [year, setYear] = useState(null);
+
+  useEffect(()=> {
+    let year = new Date().getFullYear()
+    setYear(year)
+  },[])
+
   return (
-    <footer>
-        Footer
+    <footer className='footer'>
+        <p class="text-white">© {year} Copyright - Joaquin De Luca </p>
     </footer>
   )
 }

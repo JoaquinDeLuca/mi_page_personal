5e037345aa3b999edb67e18271b835c6753c94c2
JoaquinDeLuca <joaqui222@gmail.com>
2022-11-11 18:49:49 -0300

    se agrego mas info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import './About.css'
 
 export default function About() {
   return (
-    <div>
-      About
-
+    <div className='about'>
+      <div className='aboutContainer'>
+        <h2 className='aboutH2'>Sobre Mí</h2>
+        <p className='aboutP'>
+          Soy desarrollador web y trabajo tanto en programación backend como frontend. Emocionado por mejorar mis habilidades y   aprender nuevas tecnologías. Estoy abierto a aprender y trabajar con cualquier tecnología web.
+        </p>
+        <p className='aboutP'>
+          🔭 Mi objetivo es seguir mejorando mis habilidades como desarrollador,
+          aprender nuevas tecnologías como Next.js, TypeScript, entre otras y avanzar día a día. Me gusta mucho
+          trabajar en equipo porque en conjunto es cuando más nos
+          enriquecemos de conocimientos y aprendemos unos con otros.
+        </p>
+        <p className='aboutP'>
+          🔎 Me encuentro en búsqueda activa de insertarme en el mundo IT para
+          poder crecer profesionalmente y dar soluciones a los desafíos que me
+          propongan.
+        </p>
+      </div>
     </div>
   )
 }

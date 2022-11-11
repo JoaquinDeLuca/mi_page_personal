69395629a779795b744820e69af2a1027e331975
JoaquinDeLuca <joaqui222@gmail.com>
2022-11-11 18:53:34 -0300

    se corrigio un error ortografía

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className='homeContainer'>
         <div className='homeInfo'>
-            <h1 className='homeH1'>Soy Desarrollodor Web Full Stack</h1>
-            <p>Me encanta trabajar en aplicaciones web utilizando tecnologias como MongoDB, Express, React, NodeJS</p>
+            <h1 className='homeH1'>Soy Desarrollador Web Full Stack</h1>
+            <p className='homeP'>Me encanta trabajar en aplicaciones web utilizando tecnologias como MongoDB, Express, React, NodeJS</p>
         </div>
         <img className='homeImgJoaquin' src={Joaquin} alt='Joaquin'/> 
     </div>

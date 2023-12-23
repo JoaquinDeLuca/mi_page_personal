f04b19ab62826b7c00a233e08c0c8309177c566c
joaquin <joaqui222@gmail.com>
2023-12-22 23:02:01 -0300

    se mueven componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
-import Header from "../components/Header/Header";
 import Home from "../components/Home/Home";
 import About from "../components/About/About";
 import Projects from '../components/Projects/Projects'
 import Technologies from '../components/technologies/technologies'
 import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
 
 
-export default function index() {
+export default function page() {
   return (
     <>
-      <Header />
-        <Home />
-        <About />
-        <Projects />
-        <Technologies />
-        <Contact />
-      <Footer />
+      <Home />
+      <About />
+      <Projects />
+      <Technologies />
+      <Contact />
     </>
   );
 }

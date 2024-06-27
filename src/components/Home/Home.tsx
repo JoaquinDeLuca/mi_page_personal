9f09eac628fc816e141f5e1f50f93b1e5f23730e
joaquin <joaqui222@gmail.com>
2024-06-26 23:10:43 -0300

    nueva url para el cv

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,16 +9,16 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className={style.homeContainer} id='home'>
-        <div className={style.homeInfo}>
-            <span className={style.homeSpan}>Soy Desarrollador Web Full Stack</span>
-            <p className={style.homeP}>Me encanta trabajar en aplicaciones web utilizando tecnologías como Next.js, MongoDB, Express y Node.js.</p>
-            <div className={style.homeIconContainer}>
-              <Link href='https://www.linkedin.com/in/joaquindeluca/' target='_blank'><Image width={45} className={style.homeImgIcon} src={linkedin} alt='linkedin'/></Link>
-              <Link href='https://github.com/JoaquinDeLuca' target='_blank'><Image width={45} className={style.homeImgIcon} src={github} alt='Github'/></Link>
-              <Link href='https://drive.google.com/file/d/1w_gt66Tz-Zezm80qOZLuq3EIYAPJr0MB/view?usp=sharing' target='_blank'><Image width={45} className={style.homeImgIcon} src={cv} alt='cv'/></Link>
-            </div>
+      <div className={style.homeInfo}>
+        <span className={style.homeSpan}>Soy Desarrollador Web Full Stack</span>
+        <p className={style.homeP}>Me encanta trabajar en aplicaciones web utilizando tecnologías como Next.js, MongoDB, Express y Node.js.</p>
+        <div className={style.homeIconContainer}>
+          <Link href='https://www.linkedin.com/in/joaquindeluca/' target='_blank'><Image width={45} className={style.homeImgIcon} src={linkedin} alt='linkedin' /></Link>
+          <Link href='https://github.com/JoaquinDeLuca' target='_blank'><Image width={45} className={style.homeImgIcon} src={github} alt='Github' /></Link>
+          <Link href='https://drive.google.com/file/d/1dVNZMwIyjl-tgB17pFsGTTltNn3yRG1R/view?usp=sharing' target='_blank'><Image width={45} className={style.homeImgIcon} src={cv} alt='cv' /></Link>
         </div>
-        <Image priority={true} width={320} height={320} className={style.homeImgJoaquin} src={Joaquin} alt='Joaquin'/> 
+      </div>
+      <Image priority={true} width={320} height={320} className={style.homeImgJoaquin} src={Joaquin} alt='Joaquin' />
     </div>
   )
 }

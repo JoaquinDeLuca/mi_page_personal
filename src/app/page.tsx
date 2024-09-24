685572ac8992548a9f5416ea4388e17eb3b88f12
joaquin <joaqui222@gmail.com>
2024-09-24 16:12:41 -0300

    feat: se agrega componente tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Home from "../components/Home/Home";
 import About from "../components/About/About";
-import Projects from '../components/Projects/Projects'
 import Technologies from '../components/technologies/technologies'
 import Contact from "../components/Contact/Contact";
+import Tabs from "src/components/Tabs/Tabs";
 
 
 export default function page() {
@@ -10,7 +10,7 @@ export default function page() {
     <>
       <Home />
       <About />
-      <Projects />
+      <Tabs />
       <Technologies />
       <Contact />
     </>

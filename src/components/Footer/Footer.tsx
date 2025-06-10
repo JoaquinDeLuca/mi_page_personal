cf05e4453bf39b436f01bcc6d8482bec0519c65c
joaquin <joaqui222@gmail.com>
2025-06-10 18:12:18 -0300

    chore: text change

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,18 @@
-'use client'
-import React, { useState, useEffect } from 'react'
-import style from './Footer.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import style from "./Footer.module.css";
 
 export default function Footer() {
-
   const [year, setYear] = useState(0);
 
   useEffect(() => {
-    let year = new Date().getFullYear()
-    setYear(year)
-  }, [])
+    let year = new Date().getFullYear();
+    setYear(year);
+  }, []);
 
   return (
     <footer className={style.footer}>
-      <p>{year} - Creando, Creciendo, Conquistando - Joaquin De Luca </p>
+      <p>© {year} - Creando, Creciendo, Conquistando - Joaquin De Luca </p>
     </footer>
-  )
+  );
 }

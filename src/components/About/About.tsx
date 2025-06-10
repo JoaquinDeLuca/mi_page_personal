a4bf9d0896b1915a010623c4a9c3196b7c18bedd
joaquin <joaqui222@gmail.com>
2025-06-10 18:11:23 -0300

    chore: text change

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,20 +1,33 @@
-import style from './About.module.css'
+import style from "./About.module.css";
 
 export default function About() {
   return (
-    <div className={style.about} id='about'>
+    <div className={style.about} id="about">
       <div className={style.aboutContainer}>
         <h2 className={style.aboutH2}>Sobre Mí</h2>
         <p className={style.aboutP}>
-          👨‍💻 Como desarrollador web con experiencia en el desarrollo Frontend y Backend, he trabajado en proyectos desafiantes que exigen un enfoque integral. Mi experiencia más reciente se ha centrado en el uso de tecnologías como Next.js y TypeScript. Esta versatilidad me ha permitido adaptarme a diversos entornos y enfoques. Me llena de energía la idea de ampliar mi perspectiva y aplicar mis destrezas en proyectos innovadores. Busco explorar nuevas oportunidades que me desafíen y permitan mi crecimiento profesional.
+          👨‍💻 Desarrollador Full Stack con especialización en Frontend, enfocado
+          en crear interfaces intuitivas que priorizan la experiencia del
+          usuario y desarrollar proyectos escalables. Tengo experiencia en
+          proyectos desafiantes, trabajando tanto en equipo como en forma
+          individual, utilizando principalmente React, Next.js y TypeScript. Me
+          destaco en proponer mejoras técnicas, optimizar y colaborar
+          activamente con los diferentes roles del equipos para lograr productos
+          sólidos.
         </p>
         <p className={style.aboutP}>
-          🎯 Mi objetivo profesional es el crecimiento continuo. Estoy comprometido con la mejora constante y enriquecer mi conjunto de habilidades. Además, busco fortalecer mi competencia en inglés, que considero esencial. Emocionado por integrarme a un equipo donde pueda llevar tanto mi energía como actitud positiva al proyecto.
+          🎯 Mi meta profesional es seguir creciendo, fortaleciendo mis
+          habilidades actuales, aprendiendo nuevas tecnologías y mejorando mi
+          dominio del inglés. Valoro especialmente los entornos colaborativos,
+          donde se promueve el aprendizaje mutuo y cada rol contribuye al
+          desarrollo del producto.
         </p>
         <p className={style.aboutP}>
-          🚀 En busca de encontrar un entorno en el que pueda continuar mi desarrollo profesional y contribuir de manera significativa con mis habilidades y conocimientos. Aspiro a ser parte de un equipo donde pueda marcar la diferencia y afrontar desafíos interesantes. Actualmente, estoy en busca de una oportunidad que me brinde la posibilidad de crecer como profesional y aportar al éxito del equipo o proyecto.
+          🚀 Estoy abierto a nuevas oportunidades que me permitan seguir
+          desarrollándome, asumir responsabilidades y contribuir con soluciones
+          que realmente marquen la diferencia en el equipo y en el producto.
         </p>
       </div>
     </div>
-  )
+  );
 }

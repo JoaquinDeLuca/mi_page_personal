f545c557e39fff1645dfe4ff4ba2968453f1ca65
joaquin <joaqui222@gmail.com>
2025-10-01 15:26:07 -0300

    change the title

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -1,25 +1,29 @@
-import Link from 'next/link'
-import expData from './exp.json'
-import styles from './Experience.module.css'
+import Link from "next/link";
+import expData from "./exp.json";
+import styles from "./Experience.module.css";
 
 export default function Experience() {
-    return (
-        <div className={styles.container}>
-            <p className={styles.text}>Especializado en frontend, con conocimientos sólidos en backend, lo que me permite abordar proyectos de manera integral.</p>
-            {expData.map((exp) => (
-                <div key={exp.id} className={styles.containerExp}>
-                    <div className={styles.info}>
-                        <span className={styles.pin}></span>
-                        <h3>{exp.position}</h3>
-                        <h4>{exp.place}</h4>
-                        <p>{exp.date}</p>
-                    </div>
-                    <div className={styles.description}>
-                        <p>{exp.description}</p>
-                        {exp.link && <Link className={styles.link} href={exp.link} target='_blank'>Ver más</Link>}
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>Mi trayectoria en desarrollo</p>
+      {expData.map((exp) => (
+        <div key={exp.id} className={styles.containerExp}>
+          <div className={styles.info}>
+            <span className={styles.pin}></span>
+            <h3>{exp.position}</h3>
+            <h4>{exp.place}</h4>
+            <p>{exp.date}</p>
+          </div>
+          <div className={styles.description}>
+            <p>{exp.description}</p>
+            {exp.link && (
+              <Link className={styles.link} href={exp.link} target="_blank">
+                Ver más
+              </Link>
+            )}
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
